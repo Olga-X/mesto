f7cd63f093ead2ca98a52dd8b30d966ddf4e8171
Olga Khrabrova <pochta.ox@gmail.com>
2022-04-01 23:32:49 +0300

    fix: добавить описание и обработчик «отправки» формы профиля, оптимизировать картинки

--- pages/script.js
+++ pages/script.js
@@ -1,25 +1,26 @@
+let popupProfile = document.querySelector('.popup');
+let popupProfileCloseBtn = popupProfile.querySelector('.popup__close');
 let profileEditBtn = document.querySelector('.profile__button-edit');
 let profileUserName = document.querySelector('.profile__title');
 let profileUserDescription = document.querySelector('.profile__text');
-let popupProfile = document.querySelector('.popup');
+let profileForm = popupProfile.querySelector('.form');
 let popupProfileNameInput = popupProfile.querySelector('.form__input_type_name');
 let popupProfileAboutInput = popupProfile.querySelector('.form__input_type_about');
-let popupProfileCloseBtn = popupProfile.querySelector('.popup__close');
-let profileForm = popupProfile.querySelector('.form');
 
-/* Открытие поп ап */
+
+// Открыть поп ап 
 
 function popupOpened(popup) {
   popup.classList.add('popup_opened');
 }
 
-/* Закрытие поп ап */
+// Закрыть поп ап 
 
 function popupClose(popup) {
   popup.classList.remove('popup_opened');
 }
 
-/* Открываем редактировать профиль поп ап по клик */
+// Открыть редактировать профиль поп ап по клик
 
 profileEditBtn.addEventListener('click', function() {
 
@@ -29,6 +30,22 @@ profileEditBtn.addEventListener('click', function() {
   popupOpened(popupProfile);
 })
 
-/* Закрываем поп ап по клику на крестик */
+// Закрыть поп ап по клику на крестик
 
 popupProfileCloseBtn.addEventListener('click', () => popupClose(popupProfile));
+
+// Обработчик «отправки» формы для изменения профиля
+
+function handleProfileEditSubmit(event) {
+  event.preventDefault();
+
+  profileUserName.textContent = popupProfileNameInput.value;
+  profileUserDescription.textContent = popupProfileAboutInput.value;
+
+  popupClose(popupProfile);
+
+  profileForm.reset();
+}
+// Прикрепляем обработчик к форме:он будет следить за событием “submit” - «отправка»
+
+profileForm.addEventListener('submit', handleProfileEditSubmit);
